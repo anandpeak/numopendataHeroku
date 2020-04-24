0a064b6845386acff5ef123d88cbfff103e5e52b
Anand Altankhuyag <effand9668@gmail.com>
2020-04-25 03:45:29 +0800

    added changes

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ const client = new Client({
   database: 'numdata',
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.post('/struct/departments', async (req, res) => {
   let rows = [];
   try {
